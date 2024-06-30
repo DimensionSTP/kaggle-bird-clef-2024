f3470169451c1139e4c3f64e267be724584b4da8
DimensionSTP <ddang8jh@gmail.com>
2024-07-01 07:01:02 +0900

    add: basic upload model scripts

---
+++ scripts/upload_to_hf_hub.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+path="src/postprocessing"
+is_tuned="untuned"
+strategy="ddp"
+preprocess_type="spectogram"
+upload_user="MIT"
+model_type="ast-finetuned-audioset-10-10-0.4593"
+precision=32
+batch_size=16
+epoch=10
+
+python $path/upload_to_hf_hub.py \
+    is_tuned=$is_tuned \
+    strategy=$strategy \
+    preprocess_type=$preprocess_type \
+    upload_user=$upload_user \
+    model_type=$model_type \
+    precision=$precision \
+    batch_size=$batch_size \
+    epoch=$epoch
