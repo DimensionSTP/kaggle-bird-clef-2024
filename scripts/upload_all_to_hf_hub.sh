8ce06587152ed3de90a7e311271ce6913f306e55
DimensionSTP <ddang8jh@gmail.com>
2024-12-24 18:37:04 +0900

    add: prepare and upload all scripts

---
+++ scripts/upload_all_to_hf_hub.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+path="src/postprocessing"
+is_tuned="untuned"
+strategy="ddp"
+preprocess_type="spectogram"
+upload_user="MIT"
+model_type="ast-finetuned-audioset-10-10-0.4593"
+precision=32
+batch_size=16
+model_detail="beit-base-patch16-224-pt22k-ft22k"
+
+python $path/upload_all_to_hf_hub.py \
+    is_tuned=$is_tuned \
+    strategy=$strategy \
+    preprocess_type=$preprocess_type \
+    upload_user=$upload_user \
+    model_type=$model_type \
+    precision=$precision \
+    batch_size=$batch_size \
+    model_detail=$model_detail
